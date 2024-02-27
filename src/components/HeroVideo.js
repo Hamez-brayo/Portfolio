1ead65176471320ad56e4acf466b253c92bf2840
James Brian <hamezbrayo@gmail.com>
2024-02-28 00:15:27 +0300

    home page done

---
+++ src/components/HeroVideo.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import HeroVideoSrc from "../assets/herovideo.mp4";
+import "./HeroVideoStyles.css";
+import { Link } from 'react-router-dom';
+
+const HeroVideo = () => {
+  return (
+    <div className="hero">
+      <div className="mask">
+        <video className="hero-video" autoPlay loop muted disablePictureInPicture>
+          <source src={HeroVideoSrc} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className="content">
+        <p>The name's Brian; JAMES BRIAN😌</p>
+        <h1>Coding my way through life!</h1>
+        <div>
+          <Link to="/about" className='btn'>ABOUT</Link>
+          <Link to="/contact" className='btn-light'>CONTACT</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HeroVideo;
+
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-
+import HeroVideo from "../components/HeroVideo"
+import Navbar from "../components/Navbar"
 const Home = () => {
     return (<div>
         <Navbar/>
+        <HeroVideo/>
     </div>
     );
 };
